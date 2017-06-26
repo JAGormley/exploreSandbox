71d560958a9eb6b2678ca7bf6f1c20abeef8664c
Andrew Gormley <andrewgormley@Andrews-MacBook-Pro.local>
2017-06-25 21:57:51 -0700

    bunch of new art, movement closer to done

--- Explorer.gd
+++ Explorer.gd
@@ -8,66 +8,88 @@ onready var jump = get_node("Jump")
 const GRAVITY = 500.0 # Pixels/second
 
 # Angle in degrees towards either side that the player can consider "floor"
-const FLOOR_ANGLE_TOLERANCE = 40
-const WALK_FORCE = 600
-const WALK_MIN_SPEED = 10
-const WALK_MAX_SPEED = 200
-const STOP_FORCE = 1300
+#const FLOOR_ANGLE_TOLERANCE = 40
+#const WALK_FORCE = 600
+#const WALK_MIN_SPEED = 10
+#const WALK_MAX_SPEED = 200
+#const STOP_FORCE = 1300
 const JUMP_SPEED = 200
-const JUMP_MAX_AIRBORNE_TIME = 100
-
-const SLIDE_STOP_VELOCITY = 1.0 # One pixel per second
-const SLIDE_STOP_MIN_TRAVEL = 1.0 # One pixel
+const GEN_SPEED = 1
+const WALK_SPEED = 100
+#const JUMP_MAX_AIRBORNE_TIME = 100
 
 var velocity = Vector2()
 var on_air_time = 0
 var jumping = false
-
-var prev_jump_pressed = false
+var onGround = false
 
 func _ready():
 	set_fixed_process(true)
 	set_process_input(true)
 
 func _fixed_process(delta):
-	maybeWalk(delta)
+	maybeMove(delta)
 
-func maybeWalk(delta):
-	var motion = Vector2()
-	var speed = 30
-	var moSpeed = 3
+
+
+
+
+
+
+
+
+func maybeMove(delta):
+	var GEN_SPEED = 30
 	var walkLeft = Input.is_action_pressed("move_left")
 	var walkRight = Input.is_action_pressed("move_right")
 	var jump = Input.is_action_pressed("jump")
-	var prev_jump_pressed = false
+	onGround = test_move(Vector2(0,1))
 	
 	if walkLeft:
 		walkAnim(true)
-		motion += Vector2(-moSpeed, 0)
+		velocity.x += -WALK_SPEED
 	elif walkRight:
 		walkAnim(false)
-		motion += Vector2(moSpeed, 0)
+		velocity.x += WALK_SPEED
 	
 	if (jumping and velocity.y > 0):
 		# If falling, no longer jumping
 		jumping = false
 	
-	print(on_air_time < JUMP_MAX_AIRBORNE_TIME)
-	print("jump: " + str(jump))
+	velocity.y += GRAVITY*delta
+	velocity.x = velocity.x*GEN_SPEED*delta
+	var motion_remainder = move(velocity*delta)
+	print(motion_remainder)
+#	motion_remainder += move(Vector2(0, velocity.y*delta))
 	
-	if (on_air_time < JUMP_MAX_AIRBORNE_TIME and jump and not prev_jump_pressed and not jumping):
-		velocity.y = -JUMP_SPEED
-		jumping = true
-	
-	on_air_time += delta
-	prev_jump_pressed = jump
-	
-	velocity += Vector2(0, GRAVITY)*delta
-	motion = motion*speed*delta
-	motion = move(motion)
-	move(velocity*delta)
+	#TODO: make this check for the ground instead of general collision
+	if is_colliding():
+		var normal = get_collision_normal()
+		var slope_angle = rad2deg(acos(normal.dot(Vector2(0,-1))))
+		if slope_angle > 0:
+			revert_motion()
+#			revert_motion()
+			
+		
+		velocity.y = 0
+		if (jump and not jumping):
+			velocity.y = -JUMP_SPEED
+			jumping = true
+		var final_movement = normal.slide(motion_remainder)
+		final_movement = move(final_movement)
 
-# Animate the walk
+
+
+
+
+
+
+
+
+
+
+
+# sfh = bool: set flip horizontal
 func walkAnim(sfh):
 	stand.hide()
 	walk.show()
